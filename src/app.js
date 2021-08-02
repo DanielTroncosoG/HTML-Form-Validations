550d277e811ead253d02c83d27c3c256c256bf12
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-08-02 20:16:48 +0000

    event listner

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let regexCard = /^[0-9]{13,16}$/;
+let regexCVC = /^[\d]{3}$/;
+let regNom = /[a-zA-Z]{3,}/;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function ValidateCardNumber() {
+  let cardNumber = document.querySelector("#CardNumber");
+  cardNumber.addEventListener("input", e => {
+    let textCard = e.target.value.trim();
+    // console.log(textCard);
+    if (textCard == "" || !regexCard.test(textCard)) {
+      cardNumber.classList.add("is-invalid");
+      cardNumber.classList.remove("is-valid");
+      formu.addEventListener("submit", prevenir);
+    } else {
+      cardNumber.classList.remove("is-invalid");
+      cardNumber.classList.add("is-valid");
+      formu.removeEventListener("submit", prevenir);
+    }
+  });
+}
